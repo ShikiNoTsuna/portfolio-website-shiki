7e35ec75f6a6e7310fdd25eea80271da715cc064
ShikiNoTsuna <olivier-webdev@protonmail.com>
2024-03-09 12:51:15 +0100

    fix: correction in js file

--- js/script.js
+++ js/script.js
@@ -2,11 +2,3 @@
 document.getElementById('nav-toggle').onclick = function(){
     document.getElementById("nav-content").classList.toggle("hidden");
 }
-
-let hamburger = document.getElementById('hamburgerbtn');
-    
-let mobileMenu = document.getElementById('mobileMenu');
-
-hamburger.addEventListener('click', function(){
-    mobileMenu.classList.toggle('activeMenuIcon');
-});
