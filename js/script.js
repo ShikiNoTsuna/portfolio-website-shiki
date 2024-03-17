a61a76bbdea14c37f5b2439c49c84dd16c891dc6
ShikiNoTsuna <olivier-webdev@protonmail.com>
2024-03-17 11:46:34 +0100

    feat: add typed js effect on hero tilte

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 document.getElementById('nav-toggle').onclick = function(){
     document.getElementById("nav-content").classList.toggle("hidden");
 }
+
+// typed js
+const typed = new Typed('.multiple-text', {
+    strings:['Chef de projet digital', 'Webdesigner', 'Intégrateur web'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
